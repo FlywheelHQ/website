eaf1ffa266608dd4fd2e2fc376f9e9ac9f7bf33c
Rory Byrne <rory@rory.bio>
2024-09-22 14:45:20 +0100

    feat: update project name, dependencies, and UI components
     - Changed project name from "website" to "Flywheel" in `package.json`.
     - Added `@astrojs/check` and `@fontsource-variable/roboto-flex` dependencies.
     - Removed `@fontsource-variable/bricolage-grotesque` dependency.
     - Updated `pnpm-lock.yaml` to reflect dependency changes.
     - Modified hero component layout and styles.
     - Updated navbar links and branding.
     - Deleted unused components and blog-related files.
     - Updated TailwindCSS configuration with new color palette and font settings.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,18 +1,45 @@
 /** @type {import('tailwindcss').Config} */
 const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: [
-          "Bricolage Grotesque Variable",
-          "Inter Variable",
-          "Inter",
-          ...defaultTheme.fontFamily.sans,
-        ],
-      },
+    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: [
+                    "Roboto",
+                    "Inter Variable",
+                    "Inter",
+                    ...defaultTheme.fontFamily.sans,
+                ],
+            },
+            colors: {
+                background: {
+                    DEFAULT: '#0D0119', // Midnight Blue
+                },
+                primary: {
+                    DEFAULT: '#5A3DB8', // Muted Cerulean
+                },
+                secondary: {
+                    DEFAULT: '#D7BCE8', // Sage Green
+                },
+                tertiary: {
+                    DEFAULT: '#5D5D81', // Lavender Gray
+                },
+                text: {
+                    DEFAULT: '#FEFCFD', // Pearl White
+                },
+                highlight: {
+                    DEFAULT: '#5E6A7E', // Slate Gray
+                },
+                warning: {
+                    DEFAULT: '#D4AF37', // Muted Gold
+                },
+                error: {
+                    DEFAULT: '#A3242B', // Burgundy
+                },
+            },
+        },
     },
-  },
-  plugins: [require("@tailwindcss/typography")],
+    plugins: [require("@tailwindcss/typography")],
 };
